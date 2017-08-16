4cd2ccaf64d61d3adadd90b86b9087250da25891
Navneet Gupta <Navneet-gupta01@users.noreply.github.com>
2017-08-16 20:26:29 +0400

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,11 +21,11 @@ export class LoginComponent {
   submit(): void {
     console.log('On Submit parameters to submit are : '  + JSON.stringify(this.user) );
     // this.http.get('http://localhsot:8080/');
-    if (this.user.username === 'Store' && this.user.password === '123456') {
+    if (this.user.username === 'Store' && this.user.password === 'qwerty') {
       localStorage.setItem('role', 'store');
       this.router.navigate(['store/home']);
       this.menuService.addToggleEvent.emit(true);
-    } else if (this.user.username === 'Admin' && this.user.password === '123456') {
+    } else if (this.user.username === 'Admin' && this.user.password === 'qwerty') {
       localStorage.setItem('role', 'admin');
       this.router.navigate(['admin']);
       this.menuService.addToggleEvent.emit(true);
